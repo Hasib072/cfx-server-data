e26d2e380dc242825ab295775230b0eab033433e
Hasib072 <hasibrahaman72@gmail.com>
2020-08-24 13:11:05 +0530

    LoadingScreen & Cam

---
+++ resources/[Mods]/z-loadscreen/config.js
@@ -0,0 +1,17 @@
+/* Need Help? Join my discord @ discord.gg/yWddFpQ */
+var config = {
+    /* In milliseconds */
+    "transitionInterval": 1250,
+    "imgInterval": 6000,
+    "music": true,
+    /* Insert the Youtube Video ID below */
+    "videoID": "jo4-FhqkNwQ",
+    /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
+    "musicVolume": 70,
+    "text": {
+        "title": "Kings Club",
+        "link": "Wait For a Moment",
+    },
+    /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
+    "images": ['bg0.png', 'bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg5.png', 'bg6.png', 'bg7.png','bg8.png',]
+}
